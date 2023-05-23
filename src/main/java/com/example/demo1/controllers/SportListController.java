a889cf99a7a97de3fa67ae570e299afc9763e60a
cebucmihai <mihaicebuc36@gmail.com>
2023-05-23 17:16:08 +0300

    Sport List

--- src/main/java/com/example/demo1/controllers/SportListController.java
+++ src/main/java/com/example/demo1/controllers/SportListController.java
@@ -4,12 +4,15 @@ import com.example.demo1.Main;
 import com.example.demo1.database.NitriteDB;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class SportListController {
+public class SportListController implements Initializable {
 
     @FXML
     private Label sportListLabel;
@@ -25,7 +28,26 @@ public class SportListController {
 
     @FXML
     private Button rugbyButton;
+    private Button test;
+    private static final String IDLE_BUTTON_STYLE = "-fx-background-color: transparent;";
+    private static final String HOVERED_BUTTON_STYLE = "-fx-background-color:-fx-shadow-highlight-color,#a4c2ed ;";
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //footballButton.setStyle(IDLE_BUTTON_STYLE);
+        footballButton.setOnMouseEntered(e -> footballButton.setStyle(HOVERED_BUTTON_STYLE));
+        footballButton.setOnMouseExited(e -> footballButton.setStyle(IDLE_BUTTON_STYLE));
+       // basketballButton.setStyle(IDLE_BUTTON_STYLE);
+        basketballButton.setOnMouseEntered(e -> basketballButton.setStyle(HOVERED_BUTTON_STYLE));
+        basketballButton.setOnMouseExited(e -> basketballButton.setStyle(IDLE_BUTTON_STYLE));
+       // tennisButton.setStyle(IDLE_BUTTON_STYLE);
+        tennisButton.setOnMouseEntered(e -> tennisButton.setStyle(HOVERED_BUTTON_STYLE));
+        tennisButton.setOnMouseExited(e -> tennisButton.setStyle(IDLE_BUTTON_STYLE));
+        //rugbyButton.setStyle(IDLE_BUTTON_STYLE);
+        rugbyButton.setOnMouseEntered(e -> rugbyButton.setStyle(HOVERED_BUTTON_STYLE));
+        rugbyButton.setOnMouseExited(e -> rugbyButton.setStyle(IDLE_BUTTON_STYLE));
+
+    }
     NitriteDB nitriteDB = NitriteDB.getInstance();
 
     public void toFootball(ActionEvent event) throws IOException {
