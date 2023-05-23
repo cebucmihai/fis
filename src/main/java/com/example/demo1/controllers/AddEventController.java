7114f94931d4b58b51a9d215977be78f5a58e053
cojocarucristian05 <cojocaru.cristian20@gmail.com>
2023-05-23 17:35:24 +0300

    Add sport event

---
+++ src/main/java/com/example/demo1/controllers/AddEventController.java
@@ -0,0 +1,10 @@
+package com.example.demo1.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class AddEventController {
+    @FXML
+    private Label newEvent;
+
+}
